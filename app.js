e924992e852cff2f2b7d416ca8950d0413b16184
yk0611 <hello@junkbranding.com>
2023-03-27 07:18:22 +0900

    mapで修正

--- app.js
+++ app.js
@@ -6,62 +6,64 @@ const empty = {};
 
 // xとyというキーを持ち、それぞれの値が1と2であるオブジェクトを作成してください。
 
-const obj = {
+const objEx = {
   x: 1,
   y: 2,
 };
 
 // '問題02'のオブジェクトのxの値をコンソールに出力してください。
 
-console.log(obj.x);
+console.log(objEx.x);
 
 // '問題02'のオブジェクトのyに数値3を代入してください。
 
-obj.y = 3;
+objEx.y = 3;
 
 // '問題04'のオブジェクトにキーzを追加し、それにhogeという文字列を代入してください。
 
-obj.z = 'hoge';
+objEx.z = 'hoge';
 
 // '問題05'のオブジェクトにキーnumbersを追加し、それに数値1から4の整数を１つずつ要素に持つ配列を代入してください。
 
-obj.numbers = [1, 2, 3, 4];
+objEx.numbers = [1, 2, 3, 4];
 
 // '問題06'のオブジェクトの全てのキーを要素に持つ配列を作成してください。
 
-const array = Object.keys(obj);
+const array = Object.keys(objEx);
 
 // '問題06'のオブジェクトの全ての値を要素に持つ配列を作成してください。
 
-const value = Object.values(obj);
+const value = Object.values(objEx);
 
 // '問題06'のオブジェクトのキーxとyの値の和を求めてください。
 
-let sum = 0;
-for(let i = 0; i < obj.numbers.length; i++) {
-  sum += obj.numbers[i]
-}
-console.log(sum)
+console.log(objEx.x + objEx.y)
 
 // { a: 1, b: 2, c: 3 }を[['a', 1], ['b', 2], ['c', 3]]に変換してください。
 
 const obj01 = { a: 1, b: 2, c: 3 };
-const array01 = Object.entries(obj);
-
+const array01 = Object.entries(obj01);
 console.log(array01);
+
+const arrChange = Object.keys(obj01).map(key => [key, obj01[key]]);
+console.log(arrChange);
 
 // [['a', 1], ['b', 2], ['c', 3]]を{ a: 1, b: 2, c: 3 }に変換してください。
 
 const array02 = [['a', 1], ['b', 2], ['c', 3]];
 const obj02 = Object.fromEntries(array02);
-
 console.log(obj02);
+
+const objAdd = {}
+array02.forEach(([key,value])=> {
+  objAdd[key] = value
+})
+console.log(objAdd)
 
 // { a: 1, b: 2, c: 3 }のコピーを作成してください。
 
 const originalObj = { a: 1, b: 2, c: 3 };
 const copyObj = { ...originalObj };
-
 console.log(copyObj); 
 
 // { a: 1, b: 2, c: 3 }と{ d: 4, e: 5, f: 6 }を結合した別のオブジェクトを作成してください。
@@ -69,7 +71,6 @@ console.log(copyObj);
 const obj1 = { a: 1, b: 2, c: 3 };
 const obj2 = { d: 4, e: 5, f: 6 };
 const result = { ...obj1, ...obj2 }
-
 console.log(result);
 
 // '問題13'のオブジェクトに含まれる全ての値の和を求めてください。
@@ -79,8 +80,12 @@ let sumValue = 0
 for(let value in result) {
   sumValue += result[value]
 }
-
 console.log(sumValue)
+
+const sumObj = Object.values(result).reduce((acc,current) => {
+  return acc + current
+})
+console.log(sumObj)
 
 // 任意のアルファベット列が与えられた時、その文字列に現れるアルファベットの数をカウントしオブジェクトにまとめて返す。
 
@@ -104,9 +109,25 @@ function count(word) {
   return obj;
 }
 
-// 以下のように使用してください。
 const obj = count('hello');
-console.log(obj); // { h: 1, e: 1, l: 2, o: 1}
+console.log(obj); 
+
+function countMap(word) {
+  const map = new Map();
+  // Mapオブジェクトを生成
+  for (let i = 0; i < word.length; i++) {
+    // countMap関数に入る言葉の文字の数だけ回す
+    const char = word[i];
+    // ループ変数iに対応する文字をcharに代入
+    const count = map.get(char);
+    // Mapオブジェクトからcharに対応する値を取得しcountに代入
+    map.set(char, count);
+    // Mapオブジェクトに、キーとしてchar、値としてcountのペアを追加
+  }
+  return map;
+}
+const mapObj = count('hhhellooo');
+console.log(mapObj);
 
 
 
